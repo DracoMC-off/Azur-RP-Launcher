d38bcac2404a5e6b0858802a34f68cfe6efc0eee
Draco MC <cyber.nova.offyt@gmail.com>
2025-11-07 10:26:32 +0100

    publish v1.0.0

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://admiring-blackburn.5-178-99-72.plesk.page/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://premium-launcher.azur-rp.eu/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
